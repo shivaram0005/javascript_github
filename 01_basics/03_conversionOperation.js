83070bd94ebfb099b7c191cf6c08059a3c0aad70
Shivaram0005 <shivaram8534@gmail.com>
2024-04-05 20:23:01 +0530

    completed operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+
+// ****************************** Operations **************************
+
+
+let value = 3
+let negativeValue = -value;
+// console.log(negativeValue);
+
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+
+
+let str1 = "shiva"
+let str2 = " ashu"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+console.log("1" + 2);  //12
+console.log(1 + "2");  //12
+console.log("1" + 2 + 2) //122 if first value is string then it converts entire into string
+console.log(1 + 2 + "3") //33 if first value is number then it converts entire into number + string
+
+console.log("1" + 2 + 2 + 4 + 5 + 6) //122456
+console.log(1 + 2 + 2 + 4 + 5 + "6") //146
+
+console.log(+true);   //1
+

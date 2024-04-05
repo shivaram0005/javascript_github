b264c045896e8e435afdb046870ba85d6ef82bb4
Shivaram0005 <shivaram8534@gmail.com>
2024-04-05 19:15:20 +0530

    completed dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+//"use strict"; //treat all JS code ad newer version 
+
+//alert(3 + 3);  // alert is only available in browser
+
+let name = "shiva"        // string dataType
+let age = 25              // number dataType
+let isLoggedIn = false    //boolean dataType
+let x = 999999999999999;  //bigInt dataType 
+                          //null => standlone value
+                          //undefined dataType
+                          //symbol => any thing you want to specify unique then use symbol
+
+let firstNumber;
+console.log(typeof firstNumber) //undefined
+console.log(typeof age)    
+console.log(typeof undefined)  //undefined
+console.log(typeof null)       //object               
+

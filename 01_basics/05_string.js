2f12d38d7fa3204984825b350411a9ed7331eee4
Shivaram0005 <shivaram8534@gmail.com>
2024-04-05 22:25:27 +0530

    anything to add

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+const name = "hitesh"
+const repoCount = 50
+//console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc');
+console.log(gameName.endsWith("c"));
+console.log(gameName.toUpperCase());

bac80ada604a257becfe2bd478a9fe6363bed739
Shivaram0005 <shivaram8534@gmail.com>
2024-04-06 10:25:19 +0530

    completed nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,19 @@
+const score = 400;
+
+// const balance = new Number(100);
+// console.log(balance)
+// console.log(score)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(100));
+
+
+
+// console.log(Math)
+// console.log(Math.abs(-4));   //it changes the sign
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+
+console.log(Math.random())  //ranges from 0 to 1
+console.log(Math.random() * 10);
+console.log((Math.random() * 10) + 1)

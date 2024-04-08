3122b8e0464b2c0d34d5ec89c9d5208de98d6721
Shivaram0005 <shivaram8534@gmail.com>
2024-04-08 12:04:25 +0530

    added dom

---
+++ 06_dom/one.js
@@ -0,0 +1,16 @@
+//document.querySelector(')
+
+// const myul = document.querySelector('ul')
+// console.log(myul)
+
+// const firstli = myul.querySelector('li')
+// firstli.style.color = "blue";
+
+const entireList = document.querySelectorAll('li')
+entireList[2].style.color = "blue"
+
+//with node list use forecah loop 
+//Array.from(tempClassList) to convert html collection into nodelist / array
+//innerText
+//innerContent displays all the content(even text set to display none)
+//innerHTML allow to use tags also
